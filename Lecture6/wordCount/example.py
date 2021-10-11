dad34343dc7de5ae4488e0fdfaa5c9d15a01bff6
Daniel Skjold Toft <dstoft@hotmail.com>
2021-10-11 12:06:24 +0200

    Add presentation and make line splitter to a sentence splitter

--- Lecture6/wordCount/example.py
+++ Lecture6/wordCount/example.py
@@ -1,5 +1,3 @@
-# from pyspark import SparkConf, SparkContext
-# from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
 from pyspark.sql.functions import explode, split, to_json, array, col
@@ -7,11 +5,7 @@ import locale
 locale.getdefaultlocale()
 locale.getpreferredencoding()
 
-files = "hdfs://namenode:9000/stream-in/"
-
-schema = StructType().add("value", "string")
-# wordCountSchema = StructType().add("word", "string").add("count", "int")
-
+# Create SparkSession and configure it
 spark = SparkSession.builder.appName('streamTest') \
     .config('spark.master','spark://spark-master:7077') \
     .config('spark.executor.cores', 1) \
@@ -19,37 +13,42 @@ spark = SparkSession.builder.appName('streamTest') \
     .config('spark.executor.memory', '1g') \
     .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
     .getOrCreate()
+    
+# Create a read stream from Kafka and a topic
 df = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "kafka:9092") \
-  .option("subscribe", "lines") \
+  .option("subscribe", "sentences") \
   .load()
-lines = df.selectExpr("CAST(value AS STRING)")
 
-# Split the lines into words
-words = lines.select(
+# Cast to string
+sentences = df.selectExpr("CAST(value AS STRING)")
+
+# Split the sentences into words
+# Note "explode" and "split" are SQL functions in Spark, not Python!
+words = sentences.select(
    explode(
-       split(lines.value, " ")
+       split(sentences.value, " ")
    ).alias("word")
 )
 
-# Generate running word count
+# Generate running word count by using SQL
 wordCounts = words.groupBy("word").count().sort(col('count'))
 
-# Start running the query that prints the running counts to the console
-# query = wordCounts \
-    # .writeStream \
-    # .outputMode("complete") \
-    # .format("console") \
-    # .start()
+# Only print words with more than 100 counts
+filteredWordCounts = wordCounts.where('count > 100')
+
+# Add a new column to our Dataframe, where the column name is "value" and the content is "(word, count)" as an SQL array
 columns = [col('word'), col('count')]
-mergedColumns = wordCounts.withColumn('value', array(columns))
+mergedColumns = filteredWordCounts.withColumn('value', array(columns))
+
+# Select the "mergedColumns.value" column, and convert to JSON with the alias "value", cast it to a string
+# The "value" column is required by Kafka!
+# Then create a Kafka write stream, with the output mode "complete"
 mergedColumns.select(to_json(mergedColumns.value).alias('value')).selectExpr("CAST(value AS STRING)").writeStream \
     .format('kafka') \
     .option("kafka.bootstrap.servers", "kafka:9092") \
     .option("topic", "word-counts") \
     .outputMode("complete") \
     .start().awaitTermination()
-
-# query.awaitTermination()
